7ab92268ce4dccfbdbaf24c8699da344d495dc67
Iris WANG <iris.wang@ericsson.com>
2019-03-14 16:05:36 +0800

    Add table in report.

--- MyReport.py
+++ MyReport.py
@@ -12,6 +12,7 @@ document = Document()
 
 g_pictures = []
 g_comments = []
+g_commands = []
 
 
 def create_report(product_number, tester, req_file, golden_file, dut_file):
@@ -60,8 +61,10 @@ def save_output(cmdline):
 
     global g_pictures
     global g_comments
+    global g_commands
     g_pictures.append(filename)
     g_comments.append(mp.comments)
+    g_commands.append(cmdline)
     #document.add_picture(filename)
     #document.add_paragraph('Comments: ' + mp.comments)
     #document.save(os.path.abspath(result_path + '\\DbCheckReport.docx'))
@@ -76,7 +79,7 @@ def add_conclusion():
     if not dir_existed:
         os.mkdir(result_path)
 
-    document.add_heading('Conclusion', level=0)
+    document.add_heading('\nConclusion', level=0)
     p = document.add_paragraph('Test Result:    ')
     if mp.test_result == 1:
         p.add_run('PASSED').font.color.rgb = RGBColor(0x22, 0x8B, 0x22)
@@ -87,11 +90,33 @@ def add_conclusion():
     document.save(os.path.abspath(result_path + '\\DbCheckReport.docx'))
 
 
+def add_table():
+    global g_commands
+    global g_comments
+
+    if len(g_commands) != len(g_comments):
+        print('There is error when adding table in report.')
+        return False
+
+    document.add_heading('\nResult for the commands', level=0)
+    row_num = len(g_commands)
+    table = document.add_table(rows=row_num + 1, cols=2, style="Medium Grid 1 Accent 1")
+
+    table.cell(0, 0).text = "Commands"
+    table.cell(0, 1).text = "Comments"
+
+    for row in range(1, row_num + 1):
+        table.cell(row, 0).text = g_commands[row - 1]
+        table.cell(row, 1).text = g_comments[row - 1]
+
+
 def add_pictures():
     global g_pictures
     global g_comments
     global result_path
+    global g_commands
 
+    document.add_page_break()
     document.add_heading('Pictures', level=0)
 
     if len(g_pictures) != len(g_comments):
@@ -99,8 +124,10 @@ def add_pictures():
         return False
 
     for i in range(0, len(g_pictures)):
+        document.add_paragraph('Command: ' + g_commands[i])
         document.add_paragraph('Comments: ' + g_comments[i])
         document.add_picture(g_pictures[i])
+        document.add_page_break()
         document.save(os.path.abspath(result_path + '\\DbCheckReport.docx'))
     return True
 
